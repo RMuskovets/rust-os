b18e0daf5769e21d27aaf72d39a2b537747ce968
dmytro <dymtrom@gmail.com>
2020-05-10 18:54:07 +0300

    cpuid, longmode and paging

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -15,8 +15,97 @@ global start
 section .text
 bits 32
 start:
+    mov esp, stack_top
+    call check_cpuid
+    call check_long_mode
+    call do_paging
+    call enable_paging
     mov dword [0xb8000], 0x2f4b2f4f ; OK with light-gray fg on green bg
-finish:
+    jmp hang
+
+check_cpuid:
+    pushfd
+    pop eax
+    mov ecx, eax
+    xor eax, 1 << 21
+    push eax
+    popfd
+    pushfd
+    pop eax
+    push ecx
+    popfd
+    cmp eax, ecx
+    je .no_cpuid
+    ret
+.no_cpuid:
+    mov eax, "1"
+    jmp error
+
+check_long_mode:
+    mov eax, 0x80000000
+    cpuid
+    cmp eax, 0x80000001
+    jb .no_long_mode
+    mov eax, 0x80000001
+    cpuid
+    test edx, 1 << 29
+    jz .no_long_mode
+    ret
+.no_long_mode:
+    mov al, "2"
+    jmp error
+
+do_paging:
+    mov eax, p3_table
+    or eax, 0b11
+    mov [p4_table], eax
+    mov eax, p2_table
+    or eax, 0b11
+    mov [p3_table], eax
+    mov ecx, 0
+.map_p2:
+    mov eax, 0x200000
+    mul ecx
+    or eax, 0b10000011
+    mov [p2_table + ecx * 8], eax
+    inc ecx
+    cmp ecx, 512
+    jne .map_p2
+    ret 
+
+enable_paging:
+    mov eax, p4_table
+    mov cr3, eax
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+    ret
+
+error:
+    mov dword [0xb8000], 0x4f524f45
+    mov dword [0xb8004], 0x4f3a4f52
+    mov dword [0xb8008], 0x4f204f20
+    mov byte  [0xb800a], al
+hang:
     cli
     hlt
-    jmp finish
+    jmp hang
+
+section .bss
+align 4096
+p4_table:
+    resb 4096
+p3_table:
+    resb 4096
+p2_table:
+    resb 4096
+stack_bottom:
+    resb 64*1024 ; 64 kb of stack
+stack_top:
