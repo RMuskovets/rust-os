632d19db8633adddfc3ad73cda5cea33dda7160d
RMuskovets <rmuskovets@gmail.com>
2020-05-10 22:02:05 +0300

    println! macros!

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ fn panic(_: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn rmain() { // short for "rust main"
 
-    vga::WRITER.lock().puts("hello, world!").unwrap();
+    println!("hello{}world{}", ", ", "!");
 
     loop {}
 }
