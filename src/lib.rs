d3f164c3a98f03b6ed3840761ef45b3982676449
RMuskovets <rmuskovets@gmail.com>
2020-05-10 22:08:37 +0300

    v1 testing WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![no_std]
 #![no_main]
 #![feature(exclusive_range_pattern)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "tmain"]
 
 mod vga;
 
@@ -20,5 +23,15 @@ pub extern "C" fn rmain() { // short for "rust main"
 
     println!("hello{}world{}", ", ", "!");
 
+    tmain();
+
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
