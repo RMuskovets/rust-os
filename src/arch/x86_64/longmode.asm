d8c4873be220e0b5dbaed38f6f308a807b203b06
dmytro <dymtrom@gmail.com>
2020-05-11 10:13:36 +0300

    Revert "clear screen before loading the OS"
    This reverts commit 8653b8cf48647c1676cf00123861926c65a34345.

--- src/arch/x86_64/longmode.asm
+++ src/arch/x86_64/longmode.asm
@@ -11,22 +11,9 @@ longmode_start:
     mov gs, ax
     mov rax, 0x2f592f412f4b2f4f ; "OKAY"
     mov qword [0xb8000], rax
-    ; call clear
     call rmain
     mov al, "?"
     mov byte [0xb8004], al
-; clear:
-;     push rdi
-;     push rax
-;     push rcx
-;     mov edi, 0xb8000
-;     mov rax, 0x1F201F201F201F20
-;     mov ecx, 500
-;     rep stosq
-;     pop rcx
-;     pop rax
-;     pop rdi
-;     ret
 hang:
     cli
     hlt
